4027a010e407c06991a6a093842651413a494887
Andriy Goltsev <goltsev.andriy@gmail.com>
2025-03-17 11:19:46 -0400

    Change host binding to ran dockerized.

--- framework/server/index.ts
+++ framework/server/index.ts
@@ -55,7 +55,7 @@ const runServer = async () => {
       .send(buildHtmlDoc(await renderApp(false)));
   });
 
-  fastify.listen({ port: 3000 }, (err, address) => {
+  fastify.listen({ port: 3000, host: '::' }, (err, address) => {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
